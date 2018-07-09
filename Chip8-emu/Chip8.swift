b83f143acc5c84642c52e093e1dc63eb4973d5b8
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-09 13:46:13 +0530

    Created Chip8 class and intialiser method.

---
+++ Chip8-emu/Chip8.swift
@@ -0,0 +1,48 @@
+//
+//  Chip8.swift
+//  Chip8-emu
+//
+//  Created by Raghav Vashisht on 09/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import Foundation
+
+typealias Byte = UInt8
+typealias Word = UInt16
+
+class Chip8 {
+    
+    private var stack: [Word]
+    private var stackPointer: Word
+    private var memory: [Byte]
+    private var V: [Byte]
+    private var I: Word
+    private var pc: Word
+    private var delay_timer: Byte
+    private var sound_timer: Byte
+    private var keys: [Byte]
+    private var display: [Byte]
+    private var needRedraw: Bool
+    
+    init() {
+        memory = [Byte](repeating: 0, count: 0xFFF)
+        V = [Byte](repeating: 0, count: 16)
+        I = 0x0
+        pc = 0x200
+        
+        stack = [Word](repeating: 0, count: 16)
+        stackPointer = 0
+        
+        delay_timer = 0
+        sound_timer = 0
+        
+        keys = [Byte](repeating: 0, count: 16)
+        display = [Byte](repeating: 0, count: 64*32)
+        needRedraw = false
+        
+        //loadFontset()
+    }
+    
+    
+}
