c99dc90150a9da496f4e97d9de4818b2ae3922f6
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-09 13:27:30 +0530

    Initial Commit

---
+++ Chip8-emu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chip8-emu
+//
+//  Created by Raghav Vashisht on 09/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
