0f23f0d204189d0117321c0f5f3ad8299a872506
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-09 23:50:55 +0530

    Created MainVC

--- Chip8-emu/ChipVC.swift
+++ Chip8-emu/ChipVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChipVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Chip8-emu/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  Chip8-emu
+//
+//  Created by Raghav Vashisht on 09/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
